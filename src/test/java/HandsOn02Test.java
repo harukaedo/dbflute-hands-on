66a2e6b8fea1b2062532c5ce2ad35af90cf70a96
jflute <jflute@localhost>
2026-03-13 18:05:37 +0900

    1on1ふぉろー

--- src/test/java/HandsOn02Test.java
+++ src/test/java/HandsOn02Test.java
@@ -1,3 +1,16 @@
+
+// #1on1: packageのお話をした。packageを作って配置しましょう (2026/03/13)
+// #1on1: TestCaseの継承のお話。javatryと同じで、継承することでテストできるようになる。 (2026/03/13)
+// #1on1: testメソッドの作成のお話。ここも基本的にjavatryと同じ (2026/03/13)
+// #1on1: BehaviorでのselectCount()のライブコーディング (2026/03/13)
+// (AWSのBehaviorという言葉と、ここでのBehaviorは全く別物)
+// #1on1: assertの紹介、assertTrue()とassertEquals()の違い (2026/03/13)
+
+// TODO edo gitignoreに*.log入れてもらったけど、dbflute.logがまだgitに残ってる by jflute (2026/03/13)
+// gitignoreに設定入れた時は、「これから追加されるlogファイルがignoreされる」というニュアンス。
+// なので、すでに追加されているdbflute.logは変わらずまだgit管理になる。
+// なのでなので、dbflute.logは明示的に削除コミットをしてあげないといけない。
+
 public class HandsOn02Test {
     // よくわからないが、UnitContainerTestCase を継承する時に使うらしいので作っておく
     // section2が何も進んでおらずそれぞれのファイルの役割などがあまり理解できていないため解説していただけると助かります
